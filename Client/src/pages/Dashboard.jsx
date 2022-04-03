d682a918733b9d77e8b6f211b24ac87b63ab417d
Salo Avalos <saloavalosc@gmail.com>
2022-04-02 20:49:38 -0600

    Show message when no user available
    Now if there were no users but just you, you would see a message with a random illustration

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import SyncLoader from "react-spinners/SyncLoader";
 // Components
 import BellIcon from "../components/BellIcon";
 import RingEachUser from "../components/RingEachUser";
+// import { getNoUserOnlineImagePath } from "../components/randomNoUsersOnlineImage";
 
 const Dashboard = ({ onlineUsers }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,12 +17,32 @@ const Dashboard = ({ onlineUsers }) => {
     }
   }, [onlineUsers]);
 
+  // Image paths available to use when there is no users online
+  const noUserImagePaths = [
+    "/src/assets/no-users-online/img-1.svg",
+    "/src/assets/no-users-online/img-2.svg",
+    "/src/assets/no-users-online/img-3.svg",
+  ];
+
+  // To get randomly an image path
+  const getNoUserOnlineImagePath =
+    noUserImagePaths[Math.floor(Math.random() * noUserImagePaths.length)];
+
   return (
     <>
       {isLoading ? (
         // While all we prepare all the information to show in dashboard we show a loading animation
         <div className="absolute top-1/2 left-1/2 translate-x-1/2 translate-y-1/2">
           <SyncLoader loading={isLoading} size={12} color={"#8357ff"} />
+        </div>
+      ) : onlineUsersToShow === 1 ? (
+        // If the current user that is logged in, is the only user online
+        <div>
+          <img
+            src={getNoUserOnlineImagePath}
+            alt="Eres el unico usuario en linea"
+          />
+          <span>No hay nadie a quien timbrarle</span>
         </div>
       ) : (
         <div>
